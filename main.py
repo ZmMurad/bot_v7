df433a2a666576885ac34fd940977aaedc78c1ef
MurodZM <ziyabaev_1996@mail.ru>
2022-12-03 18:22:37 +0500

    test_com

--- main.py
+++ main.py
@@ -13,8 +13,6 @@ from datetime import date
 from pyCryptoPayAPI import pyCryptoPayAPI
 import json
 vakt = date.today()
-
-
 client = pyCryptoPayAPI(api_token=TOKEN_CRYPTO)
 # Включить ведение журнала
 logging.basicConfig(
